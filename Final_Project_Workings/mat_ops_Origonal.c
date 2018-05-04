6cb863801fe2f6b5d0ea484335a42408cd549097
Lara Lufkin <lalu4246@colorado.edu>
2018-05-03 18:04:15 -0600

    Streamlined Code

--- Final_Project_Workings/mat_ops_Origonal.c
+++ Final_Project_Workings/mat_ops_Origonal.c
@@ -4,7 +4,37 @@
 #include <string.h>
 #include <time.h>
 
+//Defining the Matrix Multiplication Function
+void Matrix_Multiplication( double* Amat, double* Bmat, int Arow, int Acol, int Brow, int Bcol, double *Outmat)
+{
+	/*This Function Multiplies two Matricies Assuming the Dimensions Match*/
+	
+	//Computing Matrix Product
+	int i, j, k;
+	for (i=0;i<Arow;i++){
+		for(j=0;j<Bcol;j++){
+			for(k=0;k<Acol;k++){
+				Outmat[j+i*Bcol] += Amat[k+i*Acol]*Bmat[j+k*Bcol];
+			}
+			
+		}
+	}
+}
 
+//Defingin the Matrix Addition Function
+void Matrix_Addition( double* Amat, double* Bmat, int Elements, int Sign, double *Outmat)
+{
+	/*This Function Adds or Subtracts two Matricies Assuming the Dimensions Match*/
+
+	//Computing Matrix Addition or Subrraction
+	for (int i = 0; i<Elements; i++){
+		Outmat[i] = Amat[i]+Sign*Bmat[i];
+	}
+
+}
+
+
+// BEGIN THE MAIN FUNCTION
 int main(int argc, char * argv[])
 {
 
